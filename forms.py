3f70c952caf07daf0a1f9ec0cb10d8209b9d42c1
ikalemmon <nagatadaiki@host-M60.biochem.s.u-tokyo.ac.jp>
2022-01-11 13:47:43 +0900

    Initialize repository

---
+++ forms.py
@@ -0,0 +1,50 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+from .models import Work
+
+
+UserModel = get_user_model()
+
+
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+       super().__init__(*args, **kwargs)
+       self.fields['username'].widget.attrs['class'] = 'input'
+       self.fields['password'].widget.attrs['class'] = 'input'
+
+class UserCreateForm(UserCreationForm):
+    class Meta:
+        model = UserModel
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'input'
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = UserModel
+        fields = ('username', 'first_name', 'last_name', 'email', 'twitter')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'input'
+
+#作品投稿フォーム
+class WorkPostForm(forms.ModelForm):
+    class Meta:
+        model = Work
+        fields = ('title', 'contents', 'create_user', 'create_date')
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'input'
+
+
+#検索フォーム(まだview,model書いてない)
+#class SearchForm(Forms.form):
+#    word = forms.CharField(max_length=250)
